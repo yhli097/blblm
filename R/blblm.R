ba2d1efebb39e8b758fbb397dddb6bb1eef4bc1c
Yahui Li <liyahui097@gmail.com>
2020-06-10 03:02:09 -0700

    update Q12

--- R/blblm.R
+++ R/blblm.R
@@ -1,6 +1,8 @@
 #' @import purrr
 #' @import stats
+#' @import parallel
 #' @importFrom magrittr %>%
+#' @aliases NULL
 #' @details
 #' Linear Regression with Little Bag of Bootstraps
 "_PACKAGE"
@@ -10,13 +12,69 @@
 # from https://github.com/jennybc/googlesheets/blob/master/R/googlesheets.R
 utils::globalVariables(c("."))
 
-
+#' the main function to implement linear regression with little bag of bootstraps
+#'
+#' This function will devide the original data into several subsamples
+#' and do little bag of bootsrap.
+#'
+#' @param formula formula for linear regression
+#' @param data data for linear regression
+#' @param m the number of subsamples divided by the whole data
+#' @param B the time of little bag of bootstrap
+#' @param cluster a set of copies of R running in parallel, default as NULL only one core
+#' @param seed set.seed to make the result reproducible, default as 141
+#'
+#' @return list including estimates of coefficients and sigma
+#' in each time of little bag of bootsrap in each subsample
+#' and the formula.
 #' @export
-blblm <- function(formula, data, m = 10, B = 5000) {
-  data_list <- split_data(data, m)
-  estimates <- map(
-    data_list,
-    ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+#' @examples
+#' blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+blblm <- function(formula, file_names = NULL, n = NULL, data = NULL, m = 10, B = 5000, cluster = NULL, seed = 141) {
+  set.seed(seed)
+  if(is.null(file_names)){
+    data_list <- split_data(data, m)
+
+    if(is.null(cluster)) {
+      estimates <- map(
+        data_list,
+        ~ lm_each_subsample(formula = formula, data = ., n = nrow(data), B = B))
+    }
+
+    else {
+      # cl <- makeCluster(n_cores)
+      clusterExport(cluster, c("lm_each_subsample","lm_each_boot","lm1","blbcoef","blbsigma"))
+      estimates <- parLapply(cluster, data_list,
+            function(x) lm_each_subsample(formula = formula, data = x, n = nrow(data), B = B))
+      # stopCluster(cl)
+    }
+  }
+  else{
+    if(is.null(n)){
+      stop("ERROR: you need to input the size of the whole data!")
+    }
+    else if(is.null(cluster)){
+      estimates <- lapply(file_names, function(fname) {
+        df <- read_csv(fname, col_types = cols())
+        lm_each_subsample(formula = formula, data = df, n = n, B = B)
+      })
+    }
+    else{
+      # we will to manually load tidyverse in the workers
+      invisible(clusterEvalQ(cluster, {
+        library(tidyverse)
+        NULL
+      }))
+      clusterExport(cluster, c("lm_each_subsample","lm_each_boot","lm1","blbcoef","blbsigma"))
+
+      estimates <- parLapply(cluster, file_names, function(fname) {
+        df <- read_csv(fname, col_types = cols())
+        lm_each_subsample(formula = formula, data = df, n = n, B = B)
+      })
+    }
+
+  }
+
   res <- list(estimates = estimates, formula = formula)
   class(res) <- "blblm"
   invisible(res)
@@ -24,19 +82,38 @@ blblm <- function(formula, data, m = 10, B = 5000) {
 
 
 #' split data into m parts of approximated equal sizes
+#'
+#' @param data the original data
+#' @param m the number of subsamples
+#'
+#' @return the list of subsamples
 split_data <- function(data, m) {
   idx <- sample.int(m, nrow(data), replace = TRUE)
   data %>% split(idx)
 }
 
 
-#' compute the estimates
+#' compute the estimates for each little bag of bootstrap
+#'
+#' @param formula the formula for linear regression
+#' @param data the subsample data
+#' @param n the row number of the original data
+#' @param B the times of little bag of bootstrap
+#'
+#' @return the estimate coefficients and sigma
+#' in B little bag of bootstraps for one subsample
 lm_each_subsample <- function(formula, data, n, B) {
   replicate(B, lm_each_boot(formula, data, n), simplify = FALSE)
 }
 
 
-#' compute the regression estimates for a blb dataset
+#' compute the regression estimates for a blb dataset given a generated weight
+#'
+#' @param formula the formula for linear regression
+#' @param data the subsample data
+#' @param n the row number of original data
+#'
+#' @return the estimate of coefficients and sigma in one weighted linear regression
 lm_each_boot <- function(formula, data, n) {
   freqs <- rmultinom(1, n, rep(1, nrow(data)))
   lm1(formula, data, freqs)
@@ -44,6 +121,11 @@ lm_each_boot <- function(formula, data, n) {
 
 
 #' estimate the regression estimates based on given the number of repetitions
+#' @param formula the formula for linear regression
+#' @param data the subsample data
+#' @param freqs the weights for each case in subsample data
+#'
+#' @return the estimate of coefficients and sigma in one weighted linear regression
 lm1 <- function(formula, data, freqs) {
   # drop the original closure of formula,
   # otherwise the formula will pick a wront variable from the global scope.
@@ -54,12 +136,18 @@ lm1 <- function(formula, data, freqs) {
 
 
 #' compute the coefficients from fit
+#' @param fit the result of lm()
+#'
+#' @return the coef of fit
 blbcoef <- function(fit) {
   coef(fit)
 }
 
 
 #' compute sigma from fit
+#' @param fit the result of lm()
+#'
+#' @return sigma from fit
 blbsigma <- function(fit) {
   p <- fit$rank
   y <- model.extract(fit$model, "response")
@@ -79,39 +167,81 @@ print.blblm <- function(x, ...) {
 
 #' @export
 #' @method sigma blblm
-sigma.blblm <- function(object, confidence = FALSE, level = 0.95, ...) {
+sigma.blblm <- function(object, confidence = FALSE, level = 0.95, cluster = NULL, ...) {
   est <- object$estimates
-  sigma <- mean(map_dbl(est, ~ mean(map_dbl(., "sigma"))))
-  if (confidence) {
-    alpha <- 1 - 0.95
-    limits <- est %>%
-      map_mean(~ quantile(map_dbl(., "sigma"), c(alpha / 2, 1 - alpha / 2))) %>%
-      set_names(NULL)
-    return(c(sigma = sigma, lwr = limits[1], upr = limits[2]))
-  } else {
-    return(sigma)
+  if(is.null(cluster)){
+    sigma <- mean(map_dbl(est, ~ mean(map_dbl(., "sigma"))))
+    if (confidence) {
+      alpha <- 1 - level
+      limits <- est %>%
+        map_mean(~ quantile(map_dbl(., "sigma"), c(alpha / 2, 1 - alpha / 2))) %>%
+        set_names(NULL)
+      return(c(sigma = sigma, lwr = limits[1], upr = limits[2]))
+    } else {
+      return(sigma)
+    }
   }
+  else {
+    # cl <- makeCluster(n_cores)
+    clusterExport(cluster, c("map_dbl"))
+    sigma <- mean(parSapply(cluster, est, function(x) mean(map_dbl(x, "sigma"))))
+    if (confidence) {
+      alpha <- 1 - level
+      limits <- parSapply(cluster, est,
+                  function(x) quantile(map_dbl(x, "sigma"), c(alpha / 2, 1 - alpha / 2)))
+      limits <- parApply(cluster, limits, 1, mean)
+      limits <- as.numeric(limits)
+      return(c(sigma = sigma, lwr = limits[1], upr = limits[2]))
+    } else {
+      return(sigma)
+    }
+    # stopCluster(cl)
+  }
+
 }
 
 #' @export
 #' @method coef blblm
-coef.blblm <- function(object, ...) {
+coef.blblm <- function(object, cluster = NULL,...) {
   est <- object$estimates
-  map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
+  if(is.null(cluster)){
+    map_mean(est, ~ map_cbind(., "coef") %>% rowMeans())
+  }
+  else {
+    #cl <- makeCluster(n_cores)
+    clusterExport(cluster, c("map_cbind","%>%","map","reduce"))
+    coefs = parSapply(cluster, est, function(x) apply(map_cbind(x,"coef"),1,mean))
+    parApply(cluster, coefs, 1, mean)
+    #stopCluster(cl)
+    #coefs
+  }
 }
 
 
 #' @export
 #' @method confint blblm
-confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
+confint.blblm <- function(object, parm = NULL, level = 0.95, cluster = NULL,...) {
   if (is.null(parm)) {
     parm <- attr(terms(fit$formula), "term.labels")
   }
   alpha <- 1 - level
   est <- object$estimates
-  out <- map_rbind(parm, function(p) {
-    map_mean(est, ~ map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
-  })
+  if(is.null(cluster)){
+    out <- map_rbind(parm, function(p) {
+      map_mean(est, ~ map_dbl(., list("coef", p)) %>% quantile(c(alpha / 2, 1 - alpha / 2)))
+    })
+  }
+  else{
+    #cl <- makeCluster(n_cores)
+    clusterExport(cluster, c("map_dbl"))
+    get_interval <- function(parm){
+      coef_int = sapply(est, function(x) quantile(map_dbl(x, list("coef", parm)),c(alpha / 2, 1 - alpha / 2)))
+      apply(coef_int, 1, mean)
+    }
+    out <- parSapply(cluster, parm, get_interval)
+    out <- t(out)
+    #stopCluster(cl)
+  }
   if (is.vector(out)) {
     out <- as.matrix(t(out))
   }
@@ -121,32 +251,74 @@ confint.blblm <- function(object, parm = NULL, level = 0.95, ...) {
 
 #' @export
 #' @method predict blblm
-predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, ...) {
+predict.blblm <- function(object, new_data, confidence = FALSE, level = 0.95, cluster = NULL,...) {
   est <- object$estimates
   X <- model.matrix(reformulate(attr(terms(object$formula), "term.labels")), new_data)
-  if (confidence) {
-    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>%
-      apply(1, mean_lwr_upr, level = level) %>%
-      t())
-  } else {
-    map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>% rowMeans())
+  if(is.null(cluster)){
+    if (confidence) {
+      map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>%
+                 apply(1, mean_lwr_upr, level = level) %>%
+                 t())
+    } else {
+      map_mean(est, ~ map_cbind(., ~ X %*% .$coef) %>% rowMeans())
+    }
   }
+  else{
+    #cl <- makeCluster(n_cores)
+    if (confidence) {
+      alpha <- 1 - level
+      pred_get_interval = function(x){
+        t(apply(sapply(x, function(y) X %*% y$coef),1,
+            function(y) c( mean(y), quantile(y,c(alpha / 2, 1 - alpha / 2)))))}
+      intervals <- parLapply(cluster, est, pred_get_interval)
+      result <- Reduce("+",intervals)/length(intervals)
+      rownames(result) <- seq_len(dim(result)[1])
+      colnames(result) <- c("fit", "lwr", "upr")
+    } else {
+      pred <- parSapply(cluster, est, function(x) apply(sapply(x, function(y) X %*% y$coef),1,mean))
+      result <- parApply(cluster, pred, 1, mean)
+      names(result) <- seq_len(length(result))
+    }
+    #stopCluster(cl)
+    return(result)
+  }
+
 }
 
-
+#' get the mean as well as quantile from a data
+#' @param x list of data
+#' @param level the confidence level of confidence interval
 mean_lwr_upr <- function(x, level = 0.95) {
   alpha <- 1 - level
   c(fit = mean(x), quantile(x, c(alpha / 2, 1 - alpha / 2)) %>% set_names(c("lwr", "upr")))
 }
 
+#' get the mean of the estimates in subsamples by column
+#' @param .x lits of data
+#' @param .f function
+#' @param ... other arguments
+#'
+#' @return the mean of the list for each element
 map_mean <- function(.x, .f, ...) {
   (map(.x, .f, ...) %>% reduce(`+`)) / length(.x)
 }
 
+#' make a list as a form of column bind
+#' @param .x list of data
+#' @param .f function
+#' @param ... other arguments
+#'
+#' @return the column bind of the list
 map_cbind <- function(.x, .f, ...) {
   map(.x, .f, ...) %>% reduce(cbind)
 }
 
+#' make a list as a form of row bind
+#' @param .x list of data
+#' @param .f function
+#' @param ... other arguments
+#'
+#' @return the row bind of the list
 map_rbind <- function(.x, .f, ...) {
   map(.x, .f, ...) %>% reduce(rbind)
 }
--- README.md
+++ README.md
@@ -7,7 +7,9 @@
 
 ``` r
 library(blblm)
-fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100)
+library(parallel)
+cl = makeCluster(4)
+fit <- blblm(mpg ~ wt * hp, data = mtcars, m = 3, B = 100, cl = cl)
 coef(fit)
 #> (Intercept)          wt          hp       wt:hp 
 #> 48.88428523 -7.88702986 -0.11576659  0.02600976
@@ -27,4 +29,25 @@ predict(fit, data.frame(wt = c(2.5, 3), hp = c(150, 170)), confidence = TRUE)
 #>        fit      lwr      upr
 #> 1 21.55538 20.02457 22.48764
 #> 2 18.80785 17.50654 19.71772
+stopCluster(cl)
+
+file_names <- file.path("data/flights", list.files("data/flights"))
+m <- length(file_names)
+n <- nrow(nycflights13::flights)
+cl <- makeCluster(4)
+fit <- blblm(arr_delay ~ dep_delay + flight + distance, file_names = file_names, n = n, B = 5000, cluster = cl)
+sigma(fit, confidence = TRUE)
+#>   sigma      lwr      upr 
+#> 17.92746 17.84843 18.00715 
+confint(fit, cluster = cl)
+#>                    2.5%         97.5%
+#> dep_delay  1.015971e+00  1.0199569652
+#> flight     7.723085e-05  0.0001578702
+#> distance  -2.529749e-03 -0.0023211470
+predict(fit, data.frame(dep_delay = c(-10,-30), flight = c(1000, 500), distance = c(1500,3000) ), confidence = TRUE)
+#>       fit       lwr       upr
+#> 1 -17.2709 -17.35900 -17.18289
+#> 2 -41.3272 -41.54626 -41.11114
+
+stopCluster(cl)
 ```
